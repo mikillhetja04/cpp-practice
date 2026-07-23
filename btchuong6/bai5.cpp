096596cbb57d35f1a881976468faa927cb039308
Truong Dai Hiep <tdhvtmd@gmail.com>
2026-07-23 11:22:37 +0700

    bt chuong 5

--- btchuong6/bai5.cpp
+++ btchuong6/bai5.cpp
@@ -1,8 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n;
+
+void nhap(int a[], int &n){
+    int i;
     cin >> n;
-    int a[n];
+    for(int i = 0; i< n; i++){
+        cin >> a[i];
+    }
+}
+int timMax(int a[], int n){
+    int max = a[0];
+    for (int i = 1; i<n; i++){
+        if(a[i] > max){
+            max = a[i];
+        }
+    }
+    return max;
+}
+int main(){
+    int a[100];
+    int n;
+    nhap(a,n);
+    int max = timMax(a,n);
+    cout << max;
 }
